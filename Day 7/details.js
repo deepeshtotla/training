db56fa082ddd7dad20e19c8db76fe1d4876c8862
“deepeshtotla” <deepeshmaheshwari8@gmail.com>
2022-08-10 14:13:24 +0530

    day 7

--- Day 7/details.js
+++ Day 7/details.js
@@ -3,14 +3,20 @@ const person =
     firstName:"", 
     Age:"", 
     Address:"", 
+    City:"",
+    State:"",
+    Pincode:"",
     Hobbies:"",
     pr: function() {
-        return "Name : "+ this.firstName + "\t" + "<br><br>Age : " + this.Age+ "\t" + "<br><br>Address : " +this.Address+ "\t" + "<br><br>Hobbies : "+this.Hobbies;
+        return "Name : "+ this.firstName + "\n" + "<br><br>Age : " + this.Age+ "\n" + "<br><br>Address : " +this.Address+ "\n" + "<br><br>City : "+this.City+ "\n" + "<br><br>State : " +this.State+ "\n" + "<br><br>Pincode : "+this.Pincode+ "\n" + "<br><br>Hobbies : "+this.Hobbies; 
     }
 
     };
 person.firstName=prompt("Enter Name :");
 person.Age=prompt("Enter Age");
 person.Address=prompt("Enter Address :");
+person.City=prompt("Enter City :");
+person.State=prompt("Enter State :");
+person.Pincode=prompt("Enter Pincode :");
 person.Hobbies=prompt("Enter Hobbies:");
 document.write('<h1 style="text-align:center; color:chocolate;">'+person.pr()+'</h1>')
